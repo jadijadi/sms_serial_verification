33d29e12a49b63f39a4d2948daa1706534f98642
omid <omidekz@gmail.com>
2020-03-09 12:58:56 +0330

    create execute_and_fetchone method

--- app/utils.py
+++ app/utils.py
@@ -1,1 +1,15 @@
+"""
+  this file contains some util method
+"""
 
+def execute_and_fetchone(cursor, command, on_error_value="error"):
+  """
+  this method exce command on cursor and return zero index of fetchone method
+  """
+  if cursor is None:
+    raise Exception("cursor is None. can't run {} on None cursor".format(command))
+  try:
+    cursor.execute(command)
+    return cursor.fetchone()[0]
+  except:
+    return on_error_value
