985cc80b1bd023faaf7938b73e46f4c88acd5ae4
parsa mpsh <52998087+parsampsh@users.noreply.github.com>
2020-03-14 01:00:49 +0330

    change 40 to 400 in log_new_sms function(this has a confilict)

--- app/main.py
+++ app/main.py
@@ -373,7 +373,7 @@ def process():
     return jsonify(ret), 200
 
 def log_new_sms(status, sender, message, answer, cur):
-	if len(message) > 40:
+    if len(message) > 400:
         return
     now = time.strftime('%Y-%m-%d %H:%M:%S')
     cur.execute("INSERT INTO PROCESSED_SMS (status, sender, message, answer, date) VALUES (%s, %s, %s, %s, %s)", (status, sender, message, answer, now))
